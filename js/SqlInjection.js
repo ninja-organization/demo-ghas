e3dd7704bd9cd103b0c9e73b51d8d0b4bc74d294
uturist <eiki.sato@apriculture.ltd>
2021-08-06 16:41:54 +0900

    Update SqlInjection.js

--- js/SqlInjection.js
+++ js/SqlInjection.js
@@ -4,11 +4,11 @@ const app = require("express")(),
 
 app.get("search", function handler(req, res) {
   // BAD: the category might have SQL special characters in it
-  var query1 =
-    "SELECT ITEM,PRICE FROM PRODUCT WHERE ITEM_CATEGORY='" +
-    req.params.category +
-    "' ORDER BY PRICE";
-  pool.query(query1, [], function(err, results) {
+//   var query1 =
+//     "SELECT ITEM,PRICE FROM PRODUCT WHERE ITEM_CATEGORY='" +
+//     req.params.category +
+//     "' ORDER BY PRICE";
+//   pool.query(query1, [], function(err, results) {
     // process results
   });
 
